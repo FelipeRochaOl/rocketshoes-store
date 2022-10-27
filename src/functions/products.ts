a9f7133ce562648d3f6e5f5e02433698ffec6ef4
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-27 05:25:12 -0300

    Fix payload when selected once product

--- src/functions/products.ts
+++ src/functions/products.ts
@@ -4,13 +4,16 @@ import { products } from "../../server.json";
 const handler: Handler = async (event, context) => {
   const urlParts = event.path.split("/");
   const id = Number(urlParts[urlParts.length - 1]);
-  let response = products;
   if (!isNaN(id)) {
-    response = products.filter((product) => product.id === id);
+    const response = products.filter((product) => product.id === id);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response[0]),
+    };
   }
   return {
     statusCode: 200,
-    body: JSON.stringify(response),
+    body: JSON.stringify(products),
   };
 };
 
--- src/functions/stock.ts
+++ src/functions/stock.ts
@@ -4,13 +4,16 @@ import { stock } from "../../server.json";
 const handler: Handler = async (event, context) => {
   const urlParts = event.path.split("/");
   const id = Number(urlParts[urlParts.length - 1]);
-  let response = stock;
   if (!isNaN(id)) {
-    response = stock.filter((stock) => stock.id === id);
+    const response = stock.filter((stock) => stock.id === id);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response[0]),
+    };
   }
   return {
     statusCode: 200,
-    body: JSON.stringify(response),
+    body: JSON.stringify(stock),
   };
 };
 
